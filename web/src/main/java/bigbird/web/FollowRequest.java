a4c18d26923a8b8dc0056578bba622bba6062c7a
Dan Diephouse <dan@netzooid.com>
2009-07-22 10:06:45 -0700

    Missing file.

---
+++ web/src/main/java/bigbird/web/FollowRequest.java
@@ -0,0 +1,14 @@
+package bigbird.web;
+
+public class FollowRequest {
+    private String user;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+}
